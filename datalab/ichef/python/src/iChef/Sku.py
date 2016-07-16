3cfaf1ddac92eb5cb05ce3424c16ae98523c4726
Jose Jimenez <josejimenez@Unknown-3c-15-c2-c9-11-90.home>
2016-07-16 11:26:27 +0100

    skus

---
+++ datalab/ichef/python/src/iChef/Sku.py
@@ -0,0 +1,47 @@
+
+def prepare_sku(filename=None):
+    """
+    1- read file into dictionary
+    2- create objects:
+        - sku as a list of alternatives with main sku at top
+        - sku list
+    3- return sku list object
+    """
+    pass
+
+
+class SkuItem(object):
+    def __init__(self, sku=None, description=None, weight=0.0):
+        self.sku = sku
+        self.description = description
+        self.weight = weight
+
+class Sku(object):
+    MAX_SCORE = 100.0
+
+    def __init__(self, id=None, description=None):
+        self.id = id
+        self.description = description
+        self.alternatives = []
+        self.add_alterative(SkuItem(sku=self.id,
+                                    description=self.description,
+                                    weight=self.MAX_SCORE))
+
+    def add_alterative(self, sku_item):
+        self.alternatives.append(sku_item)
+
+
+
+class SkuCatalogue(object):
+    def __init__(self):
+        self.catalogue = {}
+
+    def add_sku(self, sku=None):
+        self.catalogue[sku.id] = sku
+
+    def has_ingredient(self, sku=None):
+        return self.ingredients.has_key(sku)
+
+    def get_ingredient(self, sku=None):
+        return self.ingredients.get(sku, None)
+
