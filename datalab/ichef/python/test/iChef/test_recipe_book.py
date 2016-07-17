ed35f01ddd00943a5ae345e7b8c00a707ac0ecdb
Jose Jimenez <josejimenez@Unknown-3c-15-c2-c9-11-90.home>
2016-07-17 16:49:57 +0100

    changed recipe tests

--- datalab/ichef/python/test/iChef/test_recipe_book.py
+++ datalab/ichef/python/test/iChef/test_recipe_book.py
@@ -2,7 +2,7 @@ import unittest
 from iChef.RecipeBook import Ingredient, Recipe, RecipeBook
 
 class RecipeBookTest(unittest.TestCase):
-    def test_create_recipe_book(self):
+    def setUp(self):
         recipe_1 = Recipe(id="recipe_001", name="test recipe 1")
         recipe_1.add_ingredients([Ingredient(sku="sku_001",
                                              description="test sku 1",
@@ -27,8 +27,11 @@ class RecipeBookTest(unittest.TestCase):
                                              weight=0.1,
                                              recipe_id="recipe_002")])
 
+        self._recipes = [recipe_1, recipe_2]
+
+    def test_create_recipe_book(self):
         recipe_book = RecipeBook()
-        recipe_book.add_recipes([recipe_1, recipe_2])
+        recipe_book.add_recipes(self._recipes)
 
         self.assertEqual(len(recipe_book.list_recipes()), 2)
         self.assertEqual(recipe_book.has_recipe("recipe_001"), True)
@@ -39,6 +42,42 @@ class RecipeBookTest(unittest.TestCase):
         self.assertEqual(len(recipe_book.get_recipe("recipe_001").list_ingredients()), 2)
         self.assertEqual(recipe_book.get_recipe("recipe_002").id, "recipe_002")
         self.assertEqual(len(recipe_book.get_recipe("recipe_002").list_ingredients()), 3)
+
+
+    def test_find_recipe_with_sku_in_ingredients_of_one_recipe(self):
+        recipe_book = RecipeBook()
+        recipe_book.add_recipes(self._recipes)
+        result = recipe_book.find_recipe_with_sku("sku_003")
+
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].sku, "sku_003")
+        self.assertEqual(result[0].description, "test sku 3")
+        self.assertEqual(result[0].weight, 0.1)
+        self.assertEqual(result[0].recipe_id, "recipe_002")
+
+
+    def test_find_recipe_with_sku_in_ingredients_of_two_recipes(self):
+        recipe_book = RecipeBook()
+        recipe_book.add_recipes(self._recipes)
+        result = recipe_book.find_recipe_with_sku("sku_001")
+
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].sku, "sku_001")
+        self.assertEqual(result[0].description, "test sku 1")
+        self.assertEqual(result[0].weight, 0.6)
+        self.assertEqual(result[0].recipe_id, "recipe_001")
+        self.assertEqual(result[1].sku, "sku_001")
+        self.assertEqual(result[1].description, "test sku 1")
+        self.assertEqual(result[1].weight, 0.5)
+        self.assertEqual(result[1].recipe_id, "recipe_002")
+
+
+    def test_find_recipe_with_sku_in_ingredients_of_no_recipe(self):
+        recipe_book = RecipeBook()
+        recipe_book.add_recipes(self._recipes)
+        result = recipe_book.find_recipe_with_sku("sku_004")
+
+        self.assertEqual(len(result), 0)
 
 
 if __name__ == '__main__':
--- datalab/ichef/python/test/iChef/test_recipe_matcher.py
+++ datalab/ichef/python/test/iChef/test_recipe_matcher.py
@@ -2,7 +2,7 @@ import unittest
 from iChef.RecipeBook import Ingredient, Recipe, RecipeBook, RecipeMatcher, MatchedIngredient
 
 class RecipeMatcherTest(unittest.TestCase):
-    def test_create_ingredient(self):
+    def setUp(self):
         recipe_1 = Recipe(id="recipe_001", name="test recipe 1")
         recipe_1.add_ingredients([Ingredient(sku="sku_001",
                                              description="test sku 1",
@@ -30,22 +30,27 @@ class RecipeMatcherTest(unittest.TestCase):
         recipe_book = RecipeBook()
         recipe_book.add_recipes([recipe_1, recipe_2])
 
-        """
+        self._recipe_book = recipe_book
 
-        TODO
 
-        implement test
+    def test_create_recipe_matcher(self):
+        recipe_matcher = RecipeMatcher(self._recipe_book)
 
-        self.assertEqual(len(recipe_book.list_recipes()), 2)
-        self.assertEqual(recipe_book.has_recipe("recipe_001"), True)
-        self.assertEqual(recipe_book.has_recipe("recipe_002"), True)
-        self.assertEqual(recipe_book.has_recipe("recipe_003"), False)
+        self.assertEqual(len(recipe_matcher.recipe_book.list_recipes()), 2)
+        self.assertEqual(recipe_matcher.recipe_book.has_recipe("recipe_001"), True)
+        self.assertEqual(recipe_matcher.recipe_book.has_recipe("recipe_002"), True)
+        self.assertEqual(recipe_matcher.recipe_book.has_recipe("recipe_003"), False)
 
-        self.assertEqual(recipe_book.get_recipe("recipe_001").id, "recipe_001")
-        self.assertEqual(len(recipe_book.get_recipe("recipe_001").list_ingredients()), 2)
-        self.assertEqual(recipe_book.get_recipe("recipe_002").id, "recipe_002")
-        self.assertEqual(len(recipe_book.get_recipe("recipe_002").list_ingredients()), 3)
-        """
+        self.assertEqual(recipe_matcher.recipe_book.get_recipe("recipe_001").id, "recipe_001")
+        self.assertEqual(len(recipe_matcher.recipe_book.get_recipe("recipe_001").list_ingredients()), 2)
+        self.assertEqual(recipe_matcher.recipe_book.get_recipe("recipe_002").id, "recipe_002")
+        self.assertEqual(len(recipe_matcher.recipe_book.get_recipe("recipe_002").list_ingredients()), 3)
+
+
+    def test_search_recipe_book(self):
+        recipe_matcher = RecipeMatcher(self._recipe_book)
+        pass
+
 
 
 if __name__ == '__main__':
