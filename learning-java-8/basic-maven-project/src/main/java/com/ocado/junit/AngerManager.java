f5f107a4c1b1b64ef5f30fb4279deebc7e0823e1
Jose Jimenez <jose.jimenez@ocado.com>
2016-12-22 16:34:00 +0000

    [java] more j-unit tests

--- learning-java-8/basic-maven-project/src/main/java/com/ocado/junit/AngerManager.java
+++ learning-java-8/basic-maven-project/src/main/java/com/ocado/junit/AngerManager.java
@@ -2,6 +2,13 @@ package com.ocado.junit;
 
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Sets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import java.util.List;
 
 /**
  * Created by jose on 22/12/16.
@@ -9,5 +16,20 @@ import com.google.common.base.Strings;
 public class AngerManager {
     public static String calmString(String s) {
         return Strings.isNullOrEmpty(s) ? s : s.replaceAll("!", "");
+    }
+
+    public static List<String> calmAttendees(List<String> crowd) {
+        if (Sets.newHashSet(crowd).size() < 2) {
+            return crowd;
+        }
+        else {
+            String chosenParty = ( new Random().nextBoolean() ) ? "Republican" : "Democrat";
+            return remove(crowd, chosenParty);
+        }
+    }
+
+    private static List<String> remove(List<String> l, String s) {
+        l.removeAll(new ArrayList<String>(Arrays.asList(s)));
+        return l;
     }
 }
--- learning-java-8/basic-maven-project/src/test/java/com/ocado/junit/AngerManagerTest.java
+++ learning-java-8/basic-maven-project/src/test/java/com/ocado/junit/AngerManagerTest.java
@@ -2,7 +2,13 @@ package com.ocado.junit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
+import com.google.common.collect.Sets;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by jose on 22/12/16.
@@ -12,8 +18,10 @@ public class AngerManagerTest {
     public void testCalmStringWithNullInput() {
         assertThat(AngerManager.calmString(null), isEmptyOrNullString());
     }
+
     @Test
     public void testCalmStringWithEmptyInput() {
+
         assertThat(AngerManager.calmString(""), isEmptyOrNullString());
     }
 
@@ -26,4 +34,27 @@ public class AngerManagerTest {
     public void testCalmStringManyExclamationMarks() {
         assertThat(AngerManager.calmString("Hello!!! World!!!"), is(equalTo("Hello World")));
     }
+
+    @Test
+    public void testCalmAttendeesEmptyRoom() {
+        List<String> crowd = new ArrayList<>();
+        List<String> result = AngerManager.calmAttendees(crowd);
+        assertThat(result.size(), is(equalTo(0)));
+    }
+
+    @Test
+    public void testCalmAttendeesMixedCrowd() {
+        List<String> crowd = new ArrayList<>(Arrays.asList("Democrat", "Republican", "Democrat", "Republican"));
+        List<String> result = AngerManager.calmAttendees(crowd);
+        assertThat(Sets.newHashSet(result).size(), is(equalTo(1)));
+    }
+
+    @Test
+    public void testCalmAttendeesUniformCrowd() {
+        List<String> crowd = new ArrayList<>(Arrays.asList("Democrat", "Democrat"));
+        List<String> result = AngerManager.calmAttendees(crowd);
+        assertThat(Sets.newHashSet(result).size(), is(equalTo(1)));
+    }
+
+
 }
